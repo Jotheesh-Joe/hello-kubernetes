040a5118953a7151de19587f334086e3bdc9c0d7
Jotheesh-Joe <58363147+Jotheesh-Joe@users.noreply.github.com>
2023-12-01 18:09:21 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,11 +3,9 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def my_home():
     return render_template('index.html', pod=os.environ['POD'], node=os.environ['NODE'], namespace=os.environ['NAMESPACE'], cluster=os.environ['ISTIO_META_CLUSTER_ID'])
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
