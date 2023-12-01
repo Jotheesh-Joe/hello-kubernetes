6087e4ead015e0b4b5361c674f1f20ca4a9b4e00
Jotheesh-Joe <joe7mailbox@gmail.com>
2023-12-01 18:00:09 +0000

    initial commit

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html', pod=os.environ['POD'], node=os.environ['NODE'], namespace=os.environ['NAMESPACE'], cluster=os.environ['ISTIO_META_CLUSTER_ID'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
